b60f32eb4a0fa6fa36e06209c8a562fd7974b635
hiroshi <hiroshi@tanuki.page>
2026-01-10 14:43:08 -0500

    asteroids: Update to Asteroids_B4.0-251229-2335
    Signed-off-by: ShantanuPC <shantanuchimote42@gmail.com>

--- qcril-database/config/0016.1_config.sql
+++ qcril-database/config/0016.1_config.sql
@@ -4,5 +4,5 @@
 */
 
 CREATE TABLE IF NOT EXISTS qcril_properties_table (property TEXT PRIMARY KEY NOT NULL, def_val TEXT, value TEXT);
-INSERT OR REPLACE INTO qcril_properties_table(property, def_val) VALUES('qcrildb_version',15.1);
+INSERT OR REPLACE INTO qcril_properties_table(property, def_val) VALUES('qcrildb_version',16.1);
 UPDATE qcril_properties_table SET def_val="0" WHERE property="persist.vendor.radio.poweron_opt";
