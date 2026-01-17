e28d2738d92559f49370e4ec5485c848235ac8ae
hiroshi <hiroshi@tanuki.page>
2026-01-17 07:45:06 -0500

    asteroids: Switch to prebuilt NFC AIDL
    For some odd reason the oss service dies even though the hardware is operating properly...
    
    Signed-off-by: ShantanuPC <shantanuchimote42@gmail.com>

--- device.mk
+++ device.mk
@@ -280,9 +280,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml
 
-PRODUCT_PACKAGES += \
-    android.hardware.nfc-service.st
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
